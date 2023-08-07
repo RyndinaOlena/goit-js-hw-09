5241d27fe957ebb99a74eaf0e93261dd5ffeb359
Ryndina Olena <ryndinaolena@gmail.com>
2023-08-07 11:33:10 +0300

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+
+const startBtn = document.querySelector('[data-start');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.querySelector('body').style.backgroundColor = getRandomHexColor()
+    }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+});
