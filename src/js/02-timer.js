708d5a090b45f54d862a6b5dbf743d136fd863f8
Ryndina Olena <ryndinaolena@gmail.com>
2023-08-11 11:31:53 +0300

    hw9

---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+import Notiflix from 'notiflix';
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+const myInputEl = document.getElementById('datetime-picker')
+const btnEl = document.querySelector('[data-start]')
+const daysEl = document.querySelector('[data-days]')
+const hoursEl = document.querySelector('[data-hours]')
+const minutesEl = document.querySelector('[data-minutes]')
+const secondsEl = document.querySelector('[data-seconds]')
+
+
+
+flatpickr(myInputEl, {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        let currentDate = Date.now()
+
+
+        if ((selectedDates[0]).getTime() < currentDate) {
+            Notiflix.Report.warning("Please choose a date in the future");
+            makeDisableDate();
+            return;
+        }
+
+        Notiflix.Notify.success("Well done");
+
+        const interval = setInterval(() => {
+            currentDate = Date.now()
+            let makeInterval = selectedDates[0] - currentDate;
+
+            let timer = convertMs(makeInterval)
+            daysEl.textContent = addLeadingZero(timer.days)
+            hoursEl.textContent = addLeadingZero(timer.hours)
+            minutesEl.textContent = addLeadingZero(timer.minutes)
+            secondsEl.textContent = addLeadingZero(timer.seconds)
+
+        }, 1000)
+    },
+
+})
+
+function makeDisableDate() {
+    btnEl.setAttribute("disabled", "disabled");
+};
+
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0')
+}
+
+
+
+
+
+
+
+
+
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  input: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
+  submit: document.querySelector('button')
 }
+refs.formEl.addEventListener('submit', handelSubmit)
+
+function createPromise({ firstDelay, step, amount }) {
+
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      let position = 0;
+      const stopInterval = setInterval(() => {
+        const shouldResolve = Math.random() > 0.3;
+        position += 1;
+        let delay = firstDelay + step * position;
+
+        if (shouldResolve) {
+          if (position === amount) {
+            clearInterval(stopInterval)
+            resolve({ position, delay })
+            // Fulfill
+          }
+        } else {
+          clearInterval(stopInterval)
+          reject({ position, delay })
+
+          // Reject
+        }
+      }, amount)
+
+    }, firstDelay)
+
+  })
+
+}
+
+function handelSubmit(e) {
+  e.preventDefault()
+  let firstDelay = Number(refs.input.value);
+  let step = Number(refs.inputStep.value);
+  let amount = Number(refs.inputAmount.value);
+
+
+
+  createPromise({ firstDelay, step, amount }).then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+  }).catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+  })
+}
+
+
