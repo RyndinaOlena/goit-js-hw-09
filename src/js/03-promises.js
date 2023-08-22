f66ca2eba97d95f040d8da37d89aff788a5ba31b
Ryndina Olena <ryndinaolena@gmail.com>
2023-08-22 18:43:10 +0300

    hw9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,96 +1,143 @@
-import Notiflix from 'notiflix';
-
-
-// setTimeout(() => {
-//   let position = 0;
+// import Notiflix from 'notiflix';
 
 
 
-//   const stopInterval = setInterval(() => {
+
+// const refs = {
+//   formEl: document.querySelector('.form'),
+//   input: document.querySelector('[name="delay"]'),
+//   inputStep: document.querySelector('[name="step"]'),
+//   inputAmount: document.querySelector('[name="amount"]'),
+//   submit: document.querySelector('button')
+// }
+// refs.formEl.addEventListener('submit', handelSubmit)
+
+// function createPromise({ firstDelay, step, position }) {
+
+//   return new Promise((resolve, reject) => {
+//     // let delay = firstDelay + step * position;
 //     const shouldResolve = Math.random() > 0.3;
-//     position += 1;
-//     let delay = firstDelay + step * position;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay })
+//       } else {
+//         reject({ position, delay })
+//       }
+//     }, firstDelay)
+//   })
+// }
 
-//     if (shouldResolve) {
+
+// function handelSubmit(e) {
+//   e.preventDefault()
+//   let firstDelay = Number(refs.input.value);
+//   let step = Number(refs.inputStep.value);
+//   let amount = Number(refs.inputAmount.value);
+//   let position = 0;
+//   makeDisableDate()
+
+//   for (let i = 1; i <= amount; i += 1) {
+//     position += 1
+//     const stopInterval = setInterval(() => {
+
+
+
+//       createPromise({ firstDelay, step, position }).then(({ position, delay }) => {
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+//       }).catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+//       });
+
 //       if (position === amount) {
 //         clearInterval(stopInterval)
-//         resolve({ position, delay })
-//         // Fulfill
+//         removeAttributeBtn()
+//         return
+
 //       }
-//     } else {
-//       clearInterval(stopInterval)
-//       reject({ position, delay })
-//       // Reject
-//     }
-//   }, step)
+
+//     }, firstDelay)
+//   }
 
 
 
 
-// }, firstDelay)
 
-const refs = {
-  formEl: document.querySelector('.form'),
-  input: document.querySelector('[name="delay"]'),
-  inputStep: document.querySelector('[name="step"]'),
-  inputAmount: document.querySelector('[name="amount"]'),
-  submit: document.querySelector('button')
-}
-refs.formEl.addEventListener('submit', handelSubmit)
 
-function createPromise({ firstDelay, step, position }) {
+//   refs.formEl.reset()
 
-  return new Promise((resolve, reject) => {
-    let delay = firstDelay + step * position;
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay })
-    } else {
-      reject({ position, delay })
+
+// }
+// function removeAttributeBtn() {
+//   refs.submit.removeAttribute("disabled", "disabled");
+// }
+
+// function makeDisableDate() {
+//   refs.submit.setAttribute("disabled", "disabled");
+// };
+
+
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const submit = document.querySelector('button')
+
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let position = 0;
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  let totalDelay = parseInt(delay.value);
+
+
+
+  submit.setAttribute("disabled", "disabled")
+  // console.log(`delay: ${delay.value}`);
+  // console.log(`step: ${step.value}`);
+  // console.log(`amount: ${amount.value}`);
+
+  for (let i = 1; i <= amount.value; i++) {
+    position += 1
+
+    createPromise(i, totalDelay).then(onSuccess).catch(onError);
+    totalDelay += parseInt(step.value);
+
+    if (position === amount.value) {
+      console.log('qwe')
+      submit.setAttribute("disabled", "disabled");
+      form.reset()
+      return
     }
 
-  })
+
+
+  }
+
+
 }
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-function handelSubmit(e) {
-  e.preventDefault()
-  let firstDelay = Number(refs.input.value);
-  let step = Number(refs.inputStep.value);
-  let amount = Number(refs.inputAmount.value);
-  let position = 0;
-  makeDisableDate()
-  setTimeout(() => {
-
-    const stopInterval = setInterval(() => {
-
-      position += 1;
-
-      createPromise({ firstDelay, step, position }).then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-      }).catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-      });
-
-      if (position === amount) {
-        clearInterval(stopInterval)
-        removeAttributeBtn()
-
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }
-    }, step)
 
-
-  }, firstDelay)
-
-  refs.formEl.reset()
-
-
-}
-function removeAttributeBtn() {
-  refs.submit.removeAttribute("disabled", "disabled");
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
 }
 
-function makeDisableDate() {
-  refs.submit.setAttribute("disabled", "disabled");
-};
+function onSuccess(resolve) {
+  Notiflix.Notify.success(resolve);
+}
 
+function onError(reject) {
+  Notiflix.Notify.failure(reject);
+}
